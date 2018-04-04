4511fd90b1f8b7f768aefba5a1c62e66935fd897
gowrimanohari3121 <35064371+gowrimanohari3121@users.noreply.github.com>
2018-04-04 11:57:44 +0530

    Add files via upload

---
+++ addsubtractcalculator/Calcu/Calc1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Add/Subtract Calculator")>
+<Assembly: AssemblyDescription("You can add or subtract Numbers.")>
+<Assembly: AssemblyCompany("Putang Ina Mo")>
+<Assembly: AssemblyProduct("Calculator")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("boboka")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6eea5bff-c9c5-490c-9578-d1bd9b6130e0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2.3.0.0")>
+<Assembly: AssemblyFileVersion("2.0.4.2")>
